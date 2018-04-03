06ff3c21c4befa4714e529974b014f2a42552c7b
Par Bhatia <parshantx@hotmail.com>
2018-04-03 00:13:56 -0400

    fixed words

--- Chess/piece.h
+++ Chess/piece.h
@@ -16,10 +16,10 @@ class Piece {
 	Pos getPos();
 	virtual bool getPassant();
 	virtual void setPassant(bool val);
-	virtual bool IsLegal(Pos newPos, vector <vector<Piece*>> pieces) = 0;
-	virtual vector<Pos> getPossibleMoves(vector<vector<Piece*>> pieces) = 0;
+    virtual bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) = 0;
+    virtual std::vector<Pos> getPossibleMoves(std::vector<std::vector<Piece*>> pieces) = 0;
 	Piece(Color c, Pos pos, bool hasMoved = false);
-	friend ostream& operator<<(ostream& out, const Piece& p);
+    friend std::ostream& operator<<(std::ostream& out, const Piece& p);
 	//No need a destructor since there is no heap allocated field and default dest is enough
 };
 
