5aae798050d99a4ab3b65a198e469e20d3487d76
Berdem <b2yildiz@uwaterloo.ca>
2018-04-03 05:33:27 -0400

    Update pawn.h

--- Chess/pawn.h
+++ Chess/pawn.h
@@ -7,10 +7,11 @@
 class Pawn : public Piece {
 	bool enPassant;
 	public:
+	bool isPawn() override;
 	bool getPassant() override;
 	void setPassant(bool val) override;
-    bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) override;
-    std::vector<Pos> getPossibleMoves(std::vector<std::vector<Piece*>> pieces) override;
+        bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) override;
+        std::vector<Pos> getPossibleMoves(std::vector<std::vector<Piece*>> pieces) override;
 	Pawn(Color c, Pos pos, bool hasMoved = false, bool enPassant = false);
 };
 
