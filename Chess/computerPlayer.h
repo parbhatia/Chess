95ee83c5ad20f0e048cae90eaf7d818eedd8a2a6
Berdem <b2yildiz@uwaterloo.ca>
2018-04-03 07:24:35 -0400

    Update computerPlayer.h

--- Chess/computerPlayer.h
+++ Chess/computerPlayer.h
@@ -3,7 +3,7 @@
 class ComputerPlayer: public Player {
 	string lvl;
 	public:
-	void move(string oldPos, string newPos, char prm = 'Q');
+	void move(string oldPos, string newPos, char prm);
 	//might not need ctor/dtor:
 	ComputerPlayer();
 	~ComputerPlayer();
