01f8188f07e26727d6fb0fd254bcb31f0fd26b67
Berdem <b2yildiz@uwaterloo.ca>
2018-04-03 03:37:46 -0400

    Update board.cc

--- Chess/board.cc
+++ Chess/board.cc
@@ -66,94 +66,94 @@ ostream &operator<<(ostream &os, const Board &b) {
     return os;
 }
 
-void Board::makeTheMove(Piece* moved, Piece* target){
-    const Pos mPos = moved->getPos(); //moving from mPos
-    const Pos tPos = target->getPos(); //moving to tPos
-    bool RegularMove = true;
-    
-    //possible enPassant capture:
-    if (((mPos.row == 3 && tPos.row == 2) || (mPos.row == 4 && tPos.row == 5)) &&
-        (abs(mPos.col - tPos.col) == 1) &&
-        (target == nullptr) &&
-        (moved->getPassant() == true)) {
+void makeTheMove(Piece* moved, Piece* target){
+	const Pos mPos = moved->getPos(); //moving from mPos
+	const Pos tPos = target->getPos(); //moving to tPos
+	bool RegularMove = true;
+
+	//possible enPassant capture:
+	if (((mPos.row == 3 && tPos.row == 2) || (mPos.row == 4 && tPos.row == 5)) &&
+		(abs(mPos.col - tPos.col) == 1) && 
+		(target == nullptr) &&
+		(moved->getPassant() == true)) {
         //We do not need to check (pieces[mPos.row][tPos.col]->getPassant() == true))
         //since we already checked that in IsLegal
-        RegularMove = false;
-        Move *m = new Move(mPos, tPos, pieces[mPos.row][tPos.col], "enPassantCapture");
-        moves.emplace_back(m);
-        target = moved; //capturing pawn moves to target
-        moved = nullptr; //old position of the capturing pawn
-        pieces[mPos.row][tPos.col] = nullptr; //position of the captured pawn
-        
-    }
-    
-    if ((moves.empty() == false) && (moves.back()->specialMove == "enPassantCapture")) {
-        //Setting enPassant field of the pieces to false after enPassantCapture
-        //We don't set enPassant field of captured piece to false
-        pieces[moves.back()->newPos.row][moves.back()->newPos.col]->setPassant(false);
-        if ((moves.back()->newPos.col + 1 == moves.back()->oldPos.col) &&
-            (moves.back()->oldPos.col - 2 >= 0)) {
-            pieces[moves.back()->oldPos.row][moves.back()->oldPos.col - 2]->setPassant(false);
-        }
-        if ((moves.back()->newPos.col - 1 == moves.back()->oldPos.col) &&
-            (moves.back()->oldPos.col + 2 <= 7)) {
-            pieces[moves.back()->oldPos.row][moves.back()->oldPos.col + 2]->setPassant(false);
-        }
-    }
-    
-    if ((moves.empty() == false) && (moves.back()->specialMove == "enPassantSetup")) {
-        //if the next move is not enPassant we set all enPassant fields to false before
-        //the other conditions
-        pieces[moves.back()->newPos.row][moves.back()->newPos.col]->setPassant(false);
-        pieces[moves.back()->newPos.row][moves.back()->newPos.col + 1]->setPassant(false);
-        pieces[moves.back()->newPos.row][moves.back()->newPos.col - 1]->setPassant(false);
-    }
-    
-    
-    if ((moved->getMoved() == false) && //possible enPassant setup (2 forward move)
-        ((mPos.row == 1 && tPos.row == 3) || (mPos.row == 6 && tPos.row == 4))) {
-        RegularMove = true;
-        Move *m = new Move{mPos, tPos, nullptr, "enPassantSetup"};
-        moves.emplace_back(m);
-        moved->setPassant(true);
-        target = moved; //2 forward move
-        moved = nullptr;
-        pieces[tPos.row][tPos.col - 1]->setPassant(true);
-        pieces[tPos.row][tPos.col + 1]->setPassant(true);
-    }
-    
-    
-    if ((moved->getMoved() == false) &&   //short-castling condition for both colors
-        ((mPos.row == 0) || (mPos.row == 7)) && (mPos.col = 4) &&
-        (tPos.row == 6)) {
-        RegularMove = true;
-        Move *m = new Move(mPos, tPos, nullptr, "Castling");
-        moves->emplace_back(m);
-        target = moved;
-        moved = nullptr;
-        pieces[mPos.row][5] = pieces[mPos.row][7]; //moves the castle to the new loc
-        pieces[mPos.row][7] = nullptr; //
-    }
-    
-    if ((moved->getMoved() == false) &&   //long-castling condition for both colors
-        ((mPos.row == 0) || (mPos.row == 7)) && (mPos.col = 4) &&
-        (tPos.row == 2)) {
-        RegularMove = true;
-        Move *m = new Move(mPos, tPos, nullptr, "Castling");
-        moves->emplace_back(m);
-        target = moved;
-        moved = nullptr;
-        pieces[mPos.row][5] = pieces[mPos.row][0]; //moves the castle to the new loc
-        pieces[mPos.row][0] = nullptr; //
-    }
-    
-    if(RegularMove == true) {
-        Move* m = new Move(mPos, tPos, target, "Regular");
-        moves.emplace_back(m); //info about move is pushed to moved vec in board
-        target = moved; //target cell points to moved piece
-        moved = nullptr; //freeing old cell
-    }
-    
+		RegularMove = false;
+		Move *m = new Move(mPos, tPos, pieces[mPos.row][tPos.col], "enPassantCapture");
+		moves->emplace_back(m);
+   		target = moved; //capturing pawn moves to target
+   		moved = nullptr; //old position of the capturing pawn
+   		pieces[mPos.row][tPos.col] = nullptr; //position of the captured pawn
+
+   	}
+
+   	if ((moves.empty() == false) && (moves.back()->specialMove == "enPassantCapture")) {
+		//Setting enPassant field of the pieces to false after enPassantCapture
+		//We don't set enPassant field of captured piece to false
+   		pieces[moves.back()->newPos.row][moves.back()->newPos.col]->setPassant(false);
+   		if ((moves.back()->newPos.col + 1 == moves.back()->oldPos.col) &&
+   			(moves.back()->oldPos.col - 2 >= 0)) {
+   			pieces[moves.back()->oldPos.row][moves.back()->oldPos.col - 2]->setPassant(false);
+   	}
+   	if ((moves.back()->newPos.col - 1 == moves.back()->oldPos.col) &&
+   		(moves.back()->oldPos.col + 2 <= 7)) {
+   		pieces[moves.back()->oldPos.row][moves.back()->oldPos.col + 2]->setPassant(false);
+  		}
+	}
+
+	if ((moves.empty() == false) && (moves.back()->specialMove == "enPassantSetup")) {
+    	//if the next move is not enPassant we set all enPassant fields to false before
+    	//the other conditions
+	pieces[moves.back()->newPos.row][moves.back()->newPos.col]->setPassant(false);
+	pieces[moves.back()->newPos.row][moves.back()->newPos.col + 1]->setPassant(false);
+	pieces[moves.back()->newPos.row][moves.back()->newPos.col - 1]->setPassant(false);
+	}
+
+
+	if ((moved->getMoved() == false) && //possible enPassant setup (2 forward move)
+		((mPos.row == 1 && tPos.row == 3) || (mPos.row == 6 && tPos.row == 4))) {
+		RegularMove = false;
+	Move *m = new Move(mPos, tPos, nullptr, "enPassantSetup");
+	moves->emplace_back(m);
+	moved->setPassant(true);
+			target = moved; //2 forward move
+			moved = nullptr; 
+			pieces[tPos.row][tPos.col - 1]->setPassant(true);
+			pieces[tPos.row][tPos.col + 1]->setPassant(true);
+		}
+
+
+	if ((moved->getMoved() == false) &&   //short-castling condition for both colors
+		((mPos.row == 0) || (mPos.row == 7)) && (mPos.col = 4) &&
+		(tPos.row == 6)) {
+		RegularMove = false;
+	Move *m = new Move(mPos, tPos, nullptr, "Castling");
+	moves->emplace_back(m);
+	target = moved;
+	moved = nullptr;
+	    pieces[mPos.row][5] = pieces[mPos.row][7]; //moves the castle to the new loc
+	    pieces[mPos.row][7] = nullptr; //
+	}
+
+	if ((moved->getMoved() == false) &&   //long-castling condition for both colors
+		((mPos.row == 0) || (mPos.row == 7)) && (mPos.col = 4) &&
+		(tPos.row == 2)) {
+		RegularMove = false;
+	Move *m = new Move(mPos, tPos, nullptr, "Castling");
+	moves->emplace_back(m);
+	target = moved;
+	moved = nullptr;
+	    pieces[mPos.row][5] = pieces[mPos.row][0]; //moves the castle to the new loc
+	    pieces[mPos.row][0] = nullptr; //
+	}
+
+	if(RegularMove == true) {
+		Move* m = new Move(mPos, tPos, target, "Regular");
+		moves.emplace_back(m); //info about move is pushed to moved vec in board
+		target = moved; //target cell points to moved piece
+		moved = nullptr; //freeing old cell
+	}
+	
 }
 
 bool Board::outOfRange(const Pos p) {
