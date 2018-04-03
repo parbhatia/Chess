34cad07e79ad6aa0433477f1df8ae74c1bd3ceea
Par Bhatia <parshantx@hotmail.com>
2018-04-03 00:07:11 -0400

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Parshant Bhatia on 2018-04-03.
+//  Copyright © 2018 Parshant Bhatia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
