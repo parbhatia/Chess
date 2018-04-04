7aa08d4dcce1c6cf52d6c203fdd0274ec00651d6
Berdem <b2yildiz@uwaterloo.ca>
2018-04-04 11:20:00 -0400

    Update computerPlayer.cc

---
+++ Chess/computerPlayer.cc
@@ -0,0 +1,73 @@
+#include <vector>
+#include <cstdlib>
+#include "computerPlayer.h"
+using namespace std;
+
+vector<vector<Pos>*> getLegalMoves() {/* returns vector that holds the list of legal move positions of a
+										 piece(if there is any) and the piece position at the end */
+	vector <vector<Pos>*> legalMoves;
+
+    for (auto &row:B->getPieces()) { //iterating through rows in board
+        for(auto &p:row){ //iterating through pieces in every row
+
+            if (p != nullptr && p->getColor() == color) {//skipping empty cells and opponent pieces
+
+	            vector <Pos> possibleMoves = p->getPossibleMoves(B->getPieces()); //getting possible moves
+	            vector <Pos> legalMovesOfPiece;
+	            int curPos = 0;
+
+                while(curPos < possibleMoves.size()) {
+                    B->makeTheMove(p->getPos(), possibleMoves[i]);
+                    if (!B->isAttacked(king->getPos())) { /* target pos added to legalMoves if it's legal */
+                        legalMovesOfPiece.emplace_back(possibleMoves[curPos]);
+                    }
+                    B->undo(); //undoing move after checking if it is legal
+                    ++curPos;
+                }
+
+                if (legalMovesOfPiece.size() > 0) {
+                	legalMovesOfPiece.emplace_back(p.getPos());
+                	legalMoves.emplace_back(&legalMovesOfPiece);
+                }
+            }
+        }
+    }
+
+return legalMoves;
+}
+
+ComputerPlayer::move(const Pos, const Pos, char prm){ //assumes lvl is valid (i.e 1-4)
+	if(lvl == "1"){
+		moveLvl1();
+	}
+	else if (lvl == "2"){
+		moveLvl2();
+	}
+	else if (lvl == "3"){
+		moveLvl3();
+	}
+	else moveLvl4();
+}
+
+ComputerPlayer::moveLvl1(){
+	vector<vector<Pos>*> legalMoves = getLegalMoves(); //getting every legal move of the player
+	int PiecePosIndex = rand() % (legalMoves.size() - 1);
+	//choosing a random target pos for the Piece stored in PiecePosIndex
+	//since piece is stored at the end, we select random index from the range [0, size-2]
+	int TargetPosIndex = rand() % (*(legalMoves[PiecePosIndex]).size() - 2);
+	Pos PiecePos = *(legalMoves[PiecePosIndex]).back();
+	Pos TargetPos = *(legalMoves[PiecePosIndex])[TargetPosIndex];
+	//getLegalMoves() function returns only legal moves, so we're safe:
+	makeTheMove(PiecePos, TargetPos);
+}
+
+ComputerPlayer::moveLvl2(){
+	//int priority = 0;
+	//vector <vector<Pos>*> legalMoves = getLegalMoves();
+	//vector <vector<Pos>> preferredMovePos(2);
+
+}
+
+ComputerPlayer::moveLvl3(){}
+
+ComputerPlayer::moveLvl4(){}
