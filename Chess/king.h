a893af378605eb52913f00e14f89e5074706b312
Berdem <b2yildiz@uwaterloo.ca>
2018-04-04 22:56:38 -0400

    Update king.h

--- Chess/king.h
+++ Chess/king.h
@@ -9,7 +9,7 @@ class King: public Piece {
 public:
     bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) override;
     std::vector<Pos> getPossibleMoves(std::vector <std::vector<Piece*>> pieces) override;
-	King(Color c, Pos pos, bool hasMoved = false);
+	King(Color c, Pos pos, int priority = 50, bool hasMoved = false);
     void print(std::ostream& out) const override;
     ~King() override;
 };
