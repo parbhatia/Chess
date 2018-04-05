2bc5d40cdedc9c844e552101d28b335db1f8188e
Berdem <b2yildiz@uwaterloo.ca>
2018-04-04 22:46:13 -0400

    Update piece.h

--- Chess/piece.h
+++ Chess/piece.h
@@ -9,8 +9,10 @@ class Piece {
 	const Color color;
 	Pos pos;
 	bool hasMoved;
+	int priority;
     virtual void print(std::ostream& out) const = 0;
 	public:
+	int getPriority();
 	void setMoved(bool val);
 	bool getMoved();
 	Color getColor() const;
@@ -21,7 +23,7 @@ class Piece {
 	virtual void setPassant(bool val);
     virtual bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) = 0;
     virtual std::vector<Pos> getPossibleMoves(std::vector<std::vector<Piece*>> pieces) = 0;
-	Piece(Color c, Pos pos, bool hasMoved = false);
+	Piece(Color c, Pos pos, bool hasMoved = false, int priority);
 	//No need a destructor since there is no heap allocated field and default dest is enough
     friend std::ostream& operator<<(std::ostream& out, const Piece &p);
     virtual ~Piece();
