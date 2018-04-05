916f0d7b30447b9f4ced8773101ec4f465424027
Berdem <b2yildiz@uwaterloo.ca>
2018-04-04 22:49:37 -0400

    Update bishop.h

--- Chess/bishop.h
+++ Chess/bishop.h
@@ -11,7 +11,7 @@ class Bishop: public Piece {
 public:
     bool IsLegal(Pos newPos, std::vector <std::vector<Piece*>> pieces) override;
     std::vector<Pos> getPossibleMoves(std::vector <std::vector<Piece*>> pieces) override;
-	Bishop(Color c, Pos Pos, bool hasMoved = false);
+	Bishop(Color c, Pos Pos, bool hasMoved = false, int priority = 3);
     void print(std::ostream& out) const override;
     ~Bishop() override;
 };
